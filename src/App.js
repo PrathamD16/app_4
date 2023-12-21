a3bf665372d859fad65a4ca40fef2eb6fa20f3e4
PrathamD16 <pratham.doke.1602@gmail.com>
2023-12-21 23:30:45 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,114 @@
 import React, { useState } from "react";
 import Dashboard from "./Components/Dashboard";
-import { Route, Router, Routes, useNavigate } from "react-router-dom";
-import LoginPage from "./Components/LoginPage";
+// import Dashboard from "./Dashboard";
 
 const App = () => {
-  return <div className="">
-    <Routes>
-      <Route path="/" element={<LoginPage />}/>
-      <Route path="/dashboard" element={<Dashboard />}/>
-    </Routes>
-  </div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [login, isLoggedIn] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    validateForm();
+  };
+
+  const validateForm = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?!.*[@].*[^\w\s]).{8,}$/;
+
+    if (!emailPattern.test(username.trim())) {
+      setEmailError("Please enter a valid email address");
+      setPasswordError("");
+    } else {
+      setEmailError("");
+    }
+
+    if (!passwordPattern.test(password.trim())) {
+      setPasswordError(
+        "Password must contain at least 1 uppercase letter, 1 number, and @ only"
+      );
+    } else {
+      setPasswordError("");
+    }
+
+    if (
+      emailPattern.test(username.trim()) &&
+      passwordPattern.test(password.trim())
+    ) {
+      if (password == "SmartServTest@123") {
+        isLoggedIn(true);
+        setPasswordError("");
+      } else {
+        setPasswordError("Please enter correct password");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div className={!login ? `` : `hidden`}>
+        <div className="min-h-screen flex justify-center items-center bg-slate-800">
+          <div className="bg-slate-900 shadow-md rounded px-8 py-8 sm:px-12 sm:py-12 w-96">
+            <h2 className="text-2xl font-semibold mb-6 text-center text-white">
+              Login
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-6">
+                <input
+                  placeholder="email"
+                  type="text"
+                  id="username"
+                  name="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:border-blue-500"
+                  required
+                />
+                {emailError && (
+                  <p className="text-red-500 text-xs mt-1 w-full h-5">
+                    {emailError}
+                  </p>
+                )}
+              </div>
+              <div className="mb-6">
+                <input
+                  placeholder="password"
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:border-blue-500"
+                  required
+                />
+                {passwordError && (
+                  <p className="text-red-500 text-xs mt-1 w-full h-5">
+                    {passwordError}
+                  </p>
+                )}
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Login
+              </button>
+            </form>
+            <div className="flex justify-center text-gray-300 mt-3 text-sm">
+              <a className="underline" href="mailto:support@smartserv.io">
+                Forgot your password
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={login ? `block` : `hidden`}>
+        <Dashboard />
+      </div>
+    </div>
+  );
 };
 
 export default App;
